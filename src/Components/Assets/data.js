43fa53f56878796d92396e083347b36b50882127
Snamude <stella.namude@gmail.com>
2025-06-20 01:14:42 -0400

    updated data

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -6,13 +6,13 @@ import p4_img from './product_4.png'
 let data_product = [
   {
     id:1,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Velvet Black Jacket",
     image:p1_img,
     new_price:50.00,
     old_price:80.50,
   },
   {id:2,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Pink Plunge Neck Blouse",
     image:p2_img,
     new_price:85.00,
     old_price:120.50,
--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,13 +6,13 @@ export const Item = (props) => {
 
     <div className='item'>
         <img src={props.image} alt="" />
-        <p>{props.name}</p>
+       <p>{props.name}</p>
         <div className="item-prices">
         <div className="item-price-new">
-            {props.new_price}
+            ${props.new_price}
         </div>
         <div className="item-price-old">
-            {props.old_price}
+            ${props.old_price}
         </div>
         </div>
     </div>
