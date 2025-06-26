0d73a8b02f3284fc2666a2e612cb4c18989bface
Snamude <stella.namude@gmail.com>
2025-06-25 21:37:23 -0400

    updated files on June 25th

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../../Context/ShopContext'
 import remove_icon from '../Assets/cart_cross_icon.png'
 
 export const CartItems = () => {
-    const {all_product, cartItems, removeFromCart} = useContext(ShopContext);
+    const {getTotalCartAmount, all_product, cartItems, removeFromCart} = useContext(ShopContext);
   return (
     <div className='cartitems'>
     <div className="cartitems-format-main">
@@ -25,13 +25,44 @@ export const CartItems = () => {
               <p>{e.name}</p>
               <p>${e.new_price}</p>
               <button className="cartitems-quantity">{cartItems[e.id]}</button>
-              <p>{e.new_price*cartItems[e.id]}</p>
-              <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+              <p>${e.new_price*cartItems[e.id]}</p>
+              <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
               </div>  
               <hr/>
           </div>
         }
+        return null;
     })}
+    <div className="cartitems-down">
+        <div className="cartitems-total">
+            <h1>Total</h1>
+        <div>
+            <div className="cartitems-total-item">
+                <p>Subtotal</p>
+                <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr/>
+            <div className="cartitems-total-item">
+                <p>Shipping Fee</p>
+                <p>Free</p>
+            </div>
+            <hr/>
+            <div className="cartitems-total-item">
+                <h3>Total</h3>
+                <h3>${getTotalCartAmount()}</h3>
+            </div>
+        </div>
+        <button>Proceed To Checkout</button>
     </div>
+   <div className="cartitems-promocode">
+    <p>Enter Promo Code</p>
+    <div className="cartitems-promobox">
+        <input type="text" placeholder='promo code' />
+        <button>Submit</button>
+    </div>
+   </div>
+   </div>
+   </div>
+   
   )
 }
